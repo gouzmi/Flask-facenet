b1e1bdf1c3dc0ad569a3f26d827217f3bf113132
vince <vincentpauwels8@gmail.com>
2019-06-24 10:48:45 +0200

    ok

---
+++ FaceNet.py
@@ -0,0 +1,70 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
+from imageio import imread
+from skimage.transform import resize
+from scipy.spatial import distance
+from keras.models import load_model
+
+cascade_path = '../model/cv2/haarcascade_frontalface_alt2.xml'
+
+def prewhiten(x):
+    if x.ndim == 4:
+        axis = (1, 2, 3)
+        size = x[0].size
+    elif x.ndim == 3:
+        axis = (0, 1, 2)
+        size = x.size
+    else:
+        raise ValueError('Dimension should be 3 or 4')
+
+    mean = np.mean(x, axis=axis, keepdims=True)
+    std = np.std(x, axis=axis, keepdims=True)
+    std_adj = np.maximum(std, 1.0/np.sqrt(size))
+    y = (x - mean) / std_adj
+    return y
+
+def l2_normalize(x, axis=-1, epsilon=1e-10):
+    output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
+    return output
+
+def load_and_align_images(filepaths, margin):
+    cascade = cv2.CascadeClassifier(cascade_path)
+    
+    aligned_images = []
+    for filepath in filepaths:
+        img = imread(filepath)
+
+        faces = cascade.detectMultiScale(img,
+                                         scaleFactor=1.1,
+                                         minNeighbors=3)
+        (x, y, w, h) = faces[0]
+        cropped = img[y-margin//2:y+h+margin//2,
+                      x-margin//2:x+w+margin//2, :]
+        aligned = resize(cropped, (image_size, image_size), mode='reflect')
+        aligned_images.append(aligned)
+            
+    return np.array(aligned_images)
+
+def calc_embs(filepaths, margin=10, batch_size=1):
+    aligned_images = prewhiten(load_and_align_images(filepaths, margin))
+    pd = []
+    for start in range(0, len(aligned_images), batch_size):
+        pd.append(model.predict_on_batch(aligned_images[start:start+batch_size]))
+    embs = l2_normalize(np.concatenate(pd))
+
+    return embs
+
+def calc_dist(img_name0, img_name1):
+    return distance.euclidean(data[img_name0]['emb'], data[img_name1]['emb'])
+
+def calc_dist_plot(img_name0, img_name1):
+    print(calc_dist(img_name0, img_name1))
+    plt.subplot(1, 2, 1)
+    plt.imshow(imread(data[img_name0]['image_filepath']))
+    plt.subplot(1, 2, 2)
+    plt.imshow(imread(data[img_name1]['image_filepath']))
---
+++ app_display_image.py
@@ -0,0 +1,64 @@
+import flask
+from uuid import uuid4
+from flask import Flask, request, render_template, send_from_directory
+from sklearn.externals import joblib
+import numpy as np
+from scipy import misc
+import cv2
+from werkzeug.utils import secure_filename
+import os
+from keras.applications import VGG16
+from keras.preprocessing import image
+from keras.models import Sequential
+from keras.applications.vgg16 import preprocess_input, decode_predictions
+import pandas as pd
+import tensorflow as tf
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    target = os.path.join(APP_ROOT, 'static/')
+    print(target)
+    if not os.path.isdir(target):
+            os.mkdir(target)
+    else:
+        print("Couldn't create upload directory: {}".format(target))
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        destination = "/".join([target, filename])
+        print ("Accept incoming file:", filename)
+        print ("Save it to:", destination)
+        upload.save(destination)
+
+    # return send_from_directory("images", filename, as_attachment=True)
+    return render_template("display.html", image_name=filename)
+
+
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+
+    #Faire les tranfos images
+
+
+
+    #Aller chercher dans le csv les images correspondantes
+
+    #Return la liste
+	return send_from_directory("static", filename)
+
+if __name__ == '__main__':	
+	# load ml model
+
+	# start api
+	app.run(debug=True)
